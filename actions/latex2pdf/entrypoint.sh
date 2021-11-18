cb182a04ec7907cc706cc74b716efd231e374ab7
mattwang44 <mattwang44@gmail.com>
2021-11-18 07:56:25 +0000

    Deploying to gh-pages from  @ d61bb3b3b5e0c3e4c5e4cc844d442b5d3b433954 🚀

--- actions/latex2pdf/entrypoint.sh
+++ actions/latex2pdf/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
-src=$1
-cd $src && sh -c "for file in *.tex; do pdflatex $file; done"
+cd $1
+for file in *.tex; do pdflatex $file; done
 
 pdf_path=$(find . -name "*.tex"  -exec basename \{\} .po \;  | tr '\n' ' ')
 echo "::set-output name=pdf_path::$pdf_path"
